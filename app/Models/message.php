3e7f0a610a8a95dd8234a1b4d0cdd8d88c308f69
kert <franciskertdn@gmail.com>
2025-09-01 21:56:20 +0800

    done

--- app/Models/message.php
+++ app/Models/message.php
@@ -3,8 +3,12 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class message extends Model
 {
-    //
+    use HasFactory;
+
+    protected $table = 'messages'; 
+    protected $fillable = ['sender', 'message'];
 }
